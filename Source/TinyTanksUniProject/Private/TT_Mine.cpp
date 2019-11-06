e18b65cc6cc2987253b225f9887e038e23d8eb7a
Thomas Howe <u1660884@unimail.hud.ac.uk>
2019-11-06 12:54:25 +0000

    COLLISION_WORKS

--- Source/TinyTanksUniProject/Private/TT_Mine.cpp
+++ Source/TinyTanksUniProject/Private/TT_Mine.cpp
@@ -18,7 +18,7 @@ ATT_Mine::ATT_Mine()
 	RootComponent = BombMesh;
 
 	MyBombMesh = CreateDefaultSubobject<UBoxComponent>(TEXT("BombComponent"));
-	MyBombMesh->InitBoxExtent(FVector(100, 100, 100));
+	MyBombMesh->InitBoxExtent(FVector(50, 50, 50));
 	MyBombMesh->SetCollisionProfileName("Hit");
 	MyBombMesh->SetupAttachment(RootComponent);
 
@@ -34,7 +34,7 @@ void ATT_Mine::BeginPlay()
 {
 	Super::BeginPlay();
 
-	DrawDebugBox(GetWorld(), GetActorLocation(), FVector(100, 100, 100), FColor::Purple, true, -1, 0, 10);
+	//DrawDebugBox(GetWorld(), GetActorLocation(), FVector(100, 100, 100), FColor::Purple, true, -1, 0, 10);
 
 	BombMesh->SetMaterial(0, FlashOff);
 }
